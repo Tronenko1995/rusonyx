3ea3fe2eb503d41564a92a66a2e2cd7dccf7765f
AlexTr999 <tronenko.aleksandr@gmail.com>
2021-03-01 18:07:51 +0200

    rusonyx 01.03.2021

--- dist/js/virtual-cloud.js
+++ dist/js/virtual-cloud.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
+
+    //скрытие и появление блока с ценами на субдомен
     $('.jsSubdomain').on('input', function () {
-        console.log($(this).val())
         if ($(this).val() != '') {
             $('.jsPriceBlock').removeClass('hide');
             $('.jsLine').removeClass('hide');
@@ -9,27 +10,60 @@ $(document).ready(function() {
             $('.jsLine').addClass('hide');
         }
     })
-    var handle = $("#custom-handle");
-    $( "#polzunok" ).slider({
-        create: function() {
-			handle.text($(this).slider("value"));
-		},
+
+    //ползунок 1
+    $('#virSlider1').slider({
         animate: "slow",
         range: "min",    
         value: 20,
+        min: 1,
         max: 190,
         slide : function(event, ui) {
-            handle.text( ui.value );
-            let val = ui.handle.style.cssText;
-            val = parseFloat(val.substr(6))
-            $(".jsSliderCounter").text(ui.value);
-            $(".jsSliderCounter").css('left',`${val}%`);
-            console.log(val)
-            // console.log('ui.handle.style.cssText', ui.handle.style.cssText) 
-            // console.log('ui.handle.style.cssText', parseInt(ui.handle.style.cssText.slice(6, 1))) 
-            // console.log('ui.handle.style.cssText', ui.handle.style.cssText.slice(2, 1)) 
+            $("#virSlider1Text").text(ui.value);
         }
     });
-    $(".jsSliderCounter").text($( "#polzunok" ).slider("value"));   
-    $(".jsSliderCounter").css('left',`${val}%`);   
+    $('#virSlider1Text').text($('#virSlider1').slider('value'));   
+
+    //ползунок 2
+    $('#virSlider2').slider({
+        animate: "slow",
+        range: "min",    
+        value: 20,
+        min: 1,
+        max: 190,
+        slide : function(event, ui) {
+            $("#virSlider2Text").text(ui.value);
+        }
+    });
+    $('#virSlider2Text').text($('#virSlider2').slider('value')); 
+    
+    //скрипт одинаковой высоты
+    function equalHeight (selector) {
+        let max_col_height = 0
+        $(selector).each(function() {
+            if ($(this).height() > max_col_height) { 
+                max_col_height = $(this).height();
+            }
+        });
+        $(selector).height(max_col_height);
+    }
+     //делаем одинаковую высоту в блоке калькулятор тарифов если ширина 1200+
+     if (document.body.clientWidth > 1185) {
+        equalHeight('.vir-calculator__block-container');
+     }
+
+    //Селект
+    $('.vir-select').on('click',function () {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+        } else {
+          $(this).addClass('open');
+        }
+    });
+    $('.vir-select').on('click', '.vir-select__item', function (e) {
+        e.stopPropagation();
+        $(this).closest('.vir-select').find('.vir-select__head').text($(this).text())
+        $(this).closest('.vir-select').find('.vir-select__input').val($(this).text());
+        $('.vir-select').removeClass('open');
+    });
 });
