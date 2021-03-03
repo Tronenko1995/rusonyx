2966b6daa3c3059efb81de2f93fdd2c3222ebeb1
AlexTr999 <tronenko.aleksandr@gmail.com>
2021-03-03 18:23:25 +0200

    +калькулятор

--- src/js/virtual-cloud.js
+++ src/js/virtual-cloud.js
@@ -1,5 +1,93 @@
 $(document).ready(function() {
 
+    //переменные
+    const price = {
+            PleskWordPressToolkit: 250,
+            PremiumEmail: {
+                user1: 290,
+                family5: 458,
+                entry10: 824,
+                starter30: 2285,
+                small50: 3567,
+                medium100: 6310,
+                large500: 27345
+            },
+            PleskImunify360: 1090,
+            SSLAlphaSSL: 2300,
+            OptimizationForBitrix: 260
+        },
+
+        PleskWordPressToolkit_checkbox = $('input[name="PleskWordPressToolkit"]'),
+        PremiumEmail_checkbox = $('input[name="PremiumEmail"]'),
+        PremiumEmailUser_select = $('input[name="PremiumEmailUser"]'),
+        PleskImunify360_checkbox = $('input[name="PleskImunify360"]'),
+        SSLAlphaSSL_checkbox = $('input[name="SSLAlphaSSL"]'),
+        OptimizationForBitrix_checkbox = $('input[name="OptimizationForBitrix"]'),
+
+        PleskWordPressToolkitText = $('#PleskWordPressToolkitText'),
+        PremiumEmailText = $('#PremiumEmailText'),
+        PleskImunify360Text = $('#PleskImunify360Text'),
+        SSLAlphaSSLText = $('#SSLAlphaSSLText'),
+        OptimizationForBitrixText = $('#OptimizationForBitrixText'),
+
+        term = $('input[name="term"]'),
+
+        totalPrice = $('#totalPrice');
+
+    let PremiumEmailUser_selectVal = $(PremiumEmailUser_select).val();
+
+
+    function calculator() {
+        let total = 0;
+        if (PleskWordPressToolkit_checkbox.is(':checked')) {
+            total = total + price.PleskWordPressToolkit;
+        }
+        if (PremiumEmail_checkbox.is(':checked')) {
+            total = total + price.PremiumEmail[PremiumEmailUser_selectVal];
+        }
+        if (PleskImunify360_checkbox.is(':checked')) {
+            total = total + price.PleskImunify360;
+        }
+        if (SSLAlphaSSL_checkbox.is(':checked')) {
+            total = total + price.SSLAlphaSSL;
+        }
+        if (OptimizationForBitrix_checkbox.is(':checked')) {
+            total = total + price.OptimizationForBitrix;
+        }
+        $(totalPrice).text(total);
+    }
+
+    $(PleskWordPressToolkit_checkbox).on('change', function() {
+        calculator();
+    })
+    $(PremiumEmail_checkbox).on('change', function() {
+        calculator();
+    })
+    $(PleskImunify360_checkbox).on('change', function() {
+        calculator();
+    })
+    $(SSLAlphaSSL_checkbox).on('change', function() {
+        calculator();
+    })
+    $(OptimizationForBitrix_checkbox).on('change', function() {
+        calculator();
+    })
+
+    function renderTextCalculator() {
+        $(PleskWordPressToolkitText).text(price.PleskWordPressToolkit);
+        $(PremiumEmailText).text(price.PremiumEmail[PremiumEmailUser_selectVal]);
+        $(PleskImunify360Text).text(price.PleskImunify360);
+        $(SSLAlphaSSLText).text(price.SSLAlphaSSL);
+        $(OptimizationForBitrixText).text(price.OptimizationForBitrix);
+    }
+    renderTextCalculator();
+
+    function recountPremiumEmail() {
+        $(PremiumEmailText).text(price.PremiumEmail[PremiumEmailUser_selectVal]);
+        calculator();
+    }
+
+
     //скрытие и появление блока с ценами на субдомен
     $('.jsSubdomain').on('input', function () {
         if ($(this).val() != '') {
@@ -11,13 +99,14 @@ $(document).ready(function() {
         }
     })
 
+
     //ползунок 1
     $('#virSlider1').slider({
         animate: "slow",
         range: "min",    
-        value: 20,
+        value: 1,
         min: 1,
-        max: 190,
+        max: 100,
         slide : function(event, ui) {
             $("#virSlider1Text").text(ui.value);
         }
@@ -28,29 +117,14 @@ $(document).ready(function() {
     $('#virSlider2').slider({
         animate: "slow",
         range: "min",    
-        value: 20,
+        value: 1,
         min: 1,
-        max: 190,
+        max: 100,
         slide : function(event, ui) {
             $("#virSlider2Text").text(ui.value);
         }
     });
     $('#virSlider2Text').text($('#virSlider2').slider('value')); 
-    
-    //скрипт одинаковой высоты
-    function equalHeight (selector) {
-        let max_col_height = 0
-        $(selector).each(function() {
-            if ($(this).height() > max_col_height) { 
-                max_col_height = $(this).height();
-            }
-        });
-        $(selector).height(max_col_height);
-    }
-     //делаем одинаковую высоту в блоке калькулятор тарифов если ширина 1200+
-     if (document.body.clientWidth > 1185) {
-        equalHeight('.vir-calculator__block-container');
-     }
 
     //Селект
     $('.vir-select').on('click',function () {
@@ -63,8 +137,16 @@ $(document).ready(function() {
     $('.vir-select').on('click', '.vir-select__item', function (e) {
         e.stopPropagation();
         $(this).closest('.vir-select').find('.vir-select__head').text($(this).text())
-        $(this).closest('.vir-select').find('.vir-select__input').val($(this).text());
+        $(this).closest('.vir-select').find('.vir-select__input').val($(this).data('value'));
         $('.vir-select').removeClass('open');
+    });
+
+    $('#premEmail').on('click', '.vir-select__item', function (e) {
+        PremiumEmailUser_selectVal = $(PremiumEmailUser_select).val()
+        recountPremiumEmail();
+    });
+    $('#term').on('click', '.vir-select__item', function (e) {
+        // recountPremiumEmail($(this).data('value'));
     });
 
     //табы
@@ -79,10 +161,12 @@ $(document).ready(function() {
         }
     })
 
+
     //Плавный скрол к элементу
     function scrollTo(element) {
         $("html, body").animate({ scrollTop: $(element).offset().top }, 1000);
     }
+
 
     //Добавить в заказ
     $('.jsAddToOrder').on('click', function(e) {
@@ -102,5 +186,31 @@ $(document).ready(function() {
 
         scrollTo(link);
         $(`.jsTab[data-tab='${tab}']`).click();
-    })
+    });
+
+    //переход к элементу
+    $('.jsGoLink').on('click', function(e) {
+        e.preventDefault();
+        const link = $(this).attr('href');
+        scrollTo(link);
+    });
+
+});
+
+$(window).on('load', function() {
+    //скрипт одинаковой высоты
+    function equalHeight (selector) {
+        let max_col_height = 0;
+        $(selector).each(function() {
+            if ($(this).height() > max_col_height) { 
+                max_col_height = $(this).height();
+            }
+        });
+        $(selector).height(max_col_height);
+    }
+
+     //делаем одинаковую высоту в блоке калькулятор тарифов если ширина 1200+
+     if (document.body.clientWidth > 1185) {
+        equalHeight('.vir-calculator__block-container');
+     }
 });
